cd8060e580e6122de7508bba54bc7ab49e1ab582
AlexBim <centuriin@yandex.ru>
2021-11-27 23:28:16 +0300

    создан класс person

---
+++ ConsoleApp/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Person
+    {
+        private int _age;
+
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value > 0 ?value : 0; PrintActualAge(); }
+        }
+
+        public Person() { }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Type: {this.GetType()}\nName: {Name}\nAge:{Age}");
+        }
+
+        public void PrintActualAge()
+        {
+            Console.WriteLine($"From actual age message: {Age}");
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,6 +7,10 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Person p1 = new Person("Vage", 19);
+            p1.PrintInfo();
+            p1.Age = -1;
+            p1.Age = 27;
         }
     }
 }
