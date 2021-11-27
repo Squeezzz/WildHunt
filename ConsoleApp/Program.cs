a4629fbd308986f517f2c8e2c423c69b3a90df6a
AlexBim <centuriin@yandex.ru>
2021-11-28 00:14:38 +0300

    fix

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Person p1 = new Person("Vage", 19);
+            Person p1 = new Person("Vage", "LastName1", 19);
             p1.PrintInfo();
             p1.Age = -1;
             p1.Age = 27;
