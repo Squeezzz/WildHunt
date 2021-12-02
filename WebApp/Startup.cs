8e612d0c5732a436f504bbb29e2903459a61d904
Denis <d.igoschin.igoshin@gmail.com>
2021-12-02 18:53:41 +0300

    tests done

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,13 +1,76 @@
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.HttpsPolicy;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.Hosting;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+
+// namespace WebApp
+// {
+//     public class Startup
+//     {
+//         public Startup(IConfiguration configuration)
+//         {
+//             Configuration = configuration;
+//         }
+
+//         public IConfiguration Configuration { get; }
+
+//         // This method gets called by the runtime. Use this method to add services to the container.
+//         public void ConfigureServices(IServiceCollection services)
+//         {
+//             services.AddControllersWithViews();
+//         }
+
+//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//         {
+//             if (env.IsDevelopment())
+//             {
+//                 app.UseDeveloperExceptionPage();
+//             }
+//             else
+//             {
+//                 app.UseExceptionHandler("/Home/Error");
+//                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//                 app.UseHsts();
+//             }
+//             app.UseHttpsRedirection();
+//             app.UseStaticFiles();
+
+//             app.UseRouting();
+
+//             app.UseAuthorization();
+
+//             app.UseEndpoints(endpoints =>
+//             {
+//                 endpoints.MapControllerRoute(
+//                     name: "default",
+//                     pattern: "{controller=Home}/{action=Index}/{id?}");
+//             });
+//         }
+//     }
+// }
+
+
+
+// NEW FILE
+
+
+
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+// using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using VueCliMiddleware;
 
 namespace WebApp
 {
@@ -23,7 +86,14 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllersWithViews();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "app-client/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,23 +105,34 @@ namespace WebApp
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSpaStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "app-client";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseVueCli(npmScript: "serve", port: 8080);
+                }
             });
         }
     }
 }
+
---
+++ WebApp/app-client/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <button @click="fetching">click</button>
+    <pre>{{ dd }}</pre>
+    <h1>{{tt}}</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  async mounted() {
+    const res = await fetch("/api/WeatherForecast");
+    const data = await res.json();
+    this.dd = data;
+  },
+  data() {
+    return {
+      dd: {},
+      tt: null,
+    };
+  },
+  name: "Home",
+  methods: {
+    async fetching() {
+      const res = await fetch("/home/m2");
+      const data = await res.json()
+      console.log(data.name)
+    },
+  },
+};
+</script>
