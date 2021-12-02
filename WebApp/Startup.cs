943ce6fc6199be5350c3d5e6e656992bf712d925
Denis <d.igoschin.igoshin@gmail.com>
2021-12-02 19:32:58 +0300

    startup for built files

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -58,16 +58,87 @@
 
 
 
-// NEW FILE
+// NEW FILE (HMR)
 
 
 
+
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.HttpsPolicy;
+// // using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.Hosting;
+// using VueCliMiddleware;
+
+// namespace WebApp
+// {
+//     public class Startup
+//     {
+//         public Startup(IConfiguration configuration)
+//         {
+//             Configuration = configuration;
+//         }
+
+//         public IConfiguration Configuration { get; }
+
+//         // This method gets called by the runtime. Use this method to add services to the container.
+//         public void ConfigureServices(IServiceCollection services)
+//         {
+
+//             services.AddControllersWithViews();
+
+//             // In production, the React files will be served from this directory
+//             services.AddSpaStaticFiles(configuration =>
+//             {
+//                 configuration.RootPath = "app-client/dist";
+//             });
+//         }
+
+//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//         {
+//             if (env.IsDevelopment())
+//             {
+//                 app.UseDeveloperExceptionPage();
+//             }
+//             else
+//             {
+//                 app.UseExceptionHandler("/Error");
+//                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//                 app.UseHsts();
+//             }
+
+//             app.UseHttpsRedirection();
+//             app.UseStaticFiles();
+//             app.UseSpaStaticFiles();
+
+//             app.UseRouting();
+
+//             app.UseEndpoints(endpoints =>
+//             {
+//                 endpoints.MapControllerRoute(
+//                     name: "default",
+//                     pattern: "{controller}/{action=Index}/{id?}");
+//             });
+
+//             app.UseSpa(spa =>
+//             {
+//                 spa.Options.SourcePath = "app-client";
+
+//                 if (env.IsDevelopment())
+//                 {
+//                     spa.UseVueCli(npmScript: "serve", port: 8080);
+//                 }
+//             });
+//         }
+//     }
+// }
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-// using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.SpaServices;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using VueCliMiddleware;
@@ -76,63 +147,35 @@ namespace WebApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllersWithViews();
-
-            // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "app-client/dist";
-            });
+            services.AddSpaStaticFiles(opt => opt.RootPath = "app-client/dist");
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-            });
+                endpoints.MapControllers();
 
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "app-client";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseVueCli(npmScript: "serve", port: 8080);
-                }
+                endpoints.MapToVueCliProxy(
+                    "{*path}",
+                    new SpaOptions { SourcePath = "app-client" },
+                    npmScript: (System.Diagnostics.Debugger.IsAttached) ? "serve" : null,
+                    regex: "Compiled successfully",
+                    forceKill: true
+                    );
             });
         }
     }
 }
-
